391a5bad144b8799587799d75ffeb50072d16c89
oplahcinski <o.plahcinski@gmail.com>
2017-07-14 07:43:52 -0400

    Convert to setuptools, use pkg-config to find poppler, add meta (#3)
    * Convert to setuptools, use pkg-config to find poppler, add meta
    
    Added some updates in the realm to try and make it easier to find when
    looking for ways to speed up pdfminer
    
    * sort, flake8, add POPPLER_ROOT environ back as default
    
    Tested on CentOS 7 vagrant box

--- README.md
+++ README.md
@@ -10,3 +10,87 @@ ldconfig
 ```
 
 Available under GPL v3 or any later version license (libpoppler is also GPL).
+
+## How to install
+
+Below or some instructions to install this package
+
+### CentOS 7 - pkg-config method
+
+Install the poppler-devel package (Tested with version 0.26.5-16.el7)
+
+    yum install poppler-devel
+
+Install cython
+    
+    pip install cython
+
+Install the repo
+    
+    pip install git+https://github.com/izderadicka/pdfparser
+
+### CentOS 7 - self compiled method
+
+Clone this repo and enter into the root folder
+
+    cd /git/repos/
+    git clone https://github.com/izderadicka/pdfparser.git
+    cd pdfparser
+
+Clone the poppler repo and install (similar to build_poppler.sh)
+    
+    yum install openjpeg2-devel libjpeg-turbo-devel
+    git clone --depth 1 git://git.freedesktop.org/git/poppler/poppler poppler_src
+    cd poppler_src
+    ./autogen.sh
+    ./configure --disable-poppler-qt4 --disable-poppler-qt5 --disable-poppler-cpp --disable-gtk-test --disable-splash-output --disable-utils
+    make
+    cp poppler/.libs/libpoppler.so.?? ../pdfparser/
+    cd ..
+    python setup.py install
+    
+ 
+ ### Debian - self compiled method
+ 
+    sh build_poppler.sh
+    cd ..
+    python setup.py install
+    
+
+
+
+
+## Speed comparisons
+
+http://zderadicka.eu/parsing-pdf-for-fun-and-profit-indeed-in-python/
+
+
+|                             | pdfreader     | pdfminer      |
+| --------------------------- | ------------- | ------------- |
+| tiny document (half page)   | 0.033s        | 0.121s        |
+| small document (5 pages)    | 0.141s        | 0.810s        |
+| medium document (55 pages)  | 1.166s        | 10.524s       |
+| large document (436 pages)  | 10.581s       | 108.095s      |
+
+
+pdfparser code used in test
+
+    import pdfparser.poppler as pdf
+    import sys
+    
+    d=pdf.Document(sys.argv[1])
+    
+    print('No of pages', d.no_of_pages)
+    for p in d:
+        print('Page', p.page_no, 'size =', p.size)
+        for f in p:
+            print(' '*1,'Flow')
+            for b in f:
+                print(' '*2,'Block', 'bbox=', b.bbox.as_tuple())
+                for l in b:
+                    print(' '*3, l.text.encode('UTF-8'), '(%0.2f, %0.2f, %0.2f, %0.2f)'% l.bbox.as_tuple())
+                    #assert l.char_fonts.comp_ratio < 1.0
+                    for i in range(len(l.text)):
+                        print(l.text[i].encode('UTF-8'), '(%0.2f, %0.2f, %0.2f, %0.2f)'% l.char_bboxes[i].as_tuple(),\
+                            l.char_fonts[i].name, l.char_fonts[i].size, l.char_fonts[i].color,)
+                    print()
--- setup.py
+++ setup.py
@@ -1,24 +1,107 @@
 #!/usr/bin/env python
 from __future__ import print_function
-from distutils.core import setup, Extension
-import os,sys
+
+import os
+import subprocess
+import sys
+
+from setuptools import Extension, setup
+
 try:
     from Cython.Build import cythonize
-    
 except ImportError:
     print('You need to install cython first - sudo pip install cython', file=sys.stderr)
     sys.exit(1)
 
-POPPLER_ROOT=os.environ.get('POPPLER_ROOT',os.path.join(os.path.dirname(__file__), 'poppler_src'))
-POPPLER_LIB_DIR= os.path.join(POPPLER_ROOT, 'poppler/.libs/')
 
-poppler_ext=Extension('pdfparser.poppler', ['pdfparser/poppler.pyx'], language='c++',
-                      extra_compile_args=[],
-                      include_dirs=[POPPLER_ROOT, os.path.join(POPPLER_ROOT, 'poppler')],
-                      library_dirs=[POPPLER_LIB_DIR],
-                      runtime_library_dirs=['$ORIGIN'],
-                      libraries=['poppler']) #,  define_macros, undef_macros, library_dirs, libraries, runtime_library_dirs, extra_objects, , extra_link_args, export_symbols, swig_opts, depends
-setup(name = 'pdfparser',
-      version = '0.1',
-      packages=['pdfparser'],
+# https://gist.github.com/smidm/ff4a2c079fed97a92e9518bd3fa4797c
+def pkgconfig(*packages, **kw):
+    """
+    Query pkg-config for library compile and linking options. Return configuration in distutils
+    Extension format.
+
+    Usage:
+
+    pkgconfig('opencv')
+
+    pkgconfig('opencv', 'libavformat')
+
+    pkgconfig('opencv', optional='--static')
+
+    pkgconfig('opencv', config=c)
+
+    returns e.g.
+
+    {'extra_compile_args': [],
+     'extra_link_args': [],
+     'include_dirs': ['/usr/include/ffmpeg'],
+     'libraries': ['avformat'],
+     'library_dirs': []}
+
+     Intended use:
+
+     distutils.core.Extension('pyextension', sources=['source.cpp'], **c)
+
+     Set PKG_CONFIG_PATH environment variable for nonstandard library locations.
+
+    based on work of Micah Dowty (http://code.activestate.com/recipes/502261-python-distutils-pkg-config/)
+    """
+    config = kw.setdefault('config', {})
+    optional_args = kw.setdefault('optional', '')
+    # { <distutils Extension arg>: [<pkg config option>, <prefix length to strip>], ...}
+    flag_map = {'include_dirs': ['--cflags-only-I', 2],
+                'library_dirs': ['--libs-only-L', 2],
+                'libraries': ['--libs-only-l', 2],
+                'extra_compile_args': ['--cflags-only-other', 0],
+                'extra_link_args': ['--libs-only-other', 0],
+                }
+    for package in packages:
+        for distutils_key, (pkg_option, n) in flag_map.items():
+            items = subprocess.check_output(['pkg-config', optional_args, pkg_option, package]).decode('utf8').split()
+            config.setdefault(distutils_key, []).extend([i[n:] for i in items])
+    return config
+
+POPPLER_ROOT = os.environ.get('POPPLER_ROOT', None)
+if POPPLER_ROOT:
+    POPPLER_LIB_DIR = os.path.join(POPPLER_ROOT, 'poppler/.libs/')
+    poppler_ext = Extension('pdfparser.poppler', ['pdfparser/poppler.pyx'], language='c++',
+                            extra_compile_args=["-std=c++11"],
+                            include_dirs=[POPPLER_ROOT, os.path.join(POPPLER_ROOT, 'poppler')],
+                            library_dirs=[POPPLER_LIB_DIR],
+                            runtime_library_dirs=['$ORIGIN'],
+                            libraries=['poppler'])
+    package_data = {'pdfparser': ['*.so.*', 'pdfparser/*.so.*']}
+else:
+    poppler_config = pkgconfig("poppler")
+    poppler_ext = Extension('pdfparser.poppler', ['pdfparser/poppler.pyx'], language='c++', **poppler_config)
+    package_data = {}
+
+
+setup(name='pdfparser',
+      version='0.1',
+      classifiers=[
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 3 - Alpha',
+
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: PDF Parsing',
+
+          'License :: OSI Approved :: GPLv3',
+
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ],
+      description="python bindings for poppler",
+      long_description="Binding for libpoppler with a focus on fast text extraction from PDF documents.",
+      keywords='poppler pdf parsing mining extracting',
+      url='https://github.com/izderadicka/pdfparser',
+      install_requires=['cython', ],
+      packages=['pdfparser', ],
+      package_data=package_data,
+      include_package_data=True,
       ext_modules=cythonize(poppler_ext))
