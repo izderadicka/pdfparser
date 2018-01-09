79fee72e4d72624657aba7754d1f95ca97df886e
Ivan <ivan.zderadicka@gmail.com>
2018-01-09 15:19:39 +0100

    Added missing dependency

--- build_poppler.sh
+++ build_poppler.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo -E apt-get update
-sudo -E apt-get install -y libtool pkg-config gettext fontconfig libfontconfig1-dev cmake libzip-dev 
+sudo -E apt-get install -y libtool pkg-config gettext fontconfig libfontconfig1-dev cmake libzip-dev libjpeg-dev
 git clone --branch poppler-0.62.0 --depth 1 https://anongit.freedesktop.org/git/poppler/poppler.git poppler_src 
 cd poppler_src/
 cmake -DENABLE_SPLASH=OFF -DENABLE_UTILS=OFF -DENABLE_LIBOPENJPEG=none .
